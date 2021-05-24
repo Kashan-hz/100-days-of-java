c75751b913b3080cd2b086687f06f73506b7fdd2
Kashan Haider <kashanhaider7233@gmail.com>
2021-05-24 23:06:28 +0530

    Initial commit

---
+++ day1/src/swap.java
@@ -0,0 +1,18 @@
+public class swap {
+    public static void main(String args[]){
+        int first=10,second=12;
+        System.out.println("Before Swap");
+        System.out.println("First number = "+first);
+        System.out.println("Second number = "+second);
+
+        int temp = first;
+        first = second;
+        second = temp;
+
+        System.out.println("Before Swap");
+        System.out.println("First number = "+first);
+        System.out.println("Second number = "+second);
+
+
+    }
+}
