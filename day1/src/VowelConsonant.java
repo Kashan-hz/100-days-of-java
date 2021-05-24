75af6497be7fc03e7ea9311f8c27a85626c4396c
Kashan Haider <kashanhaider7233@gmail.com>
2021-05-24 23:20:37 +0530

    updated

--- day1/src/VowelConsonant.java
+++ day1/src/VowelConsonant.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 
 public class VowelConsonant {
     public static void main(String args[]){
-        char ch = 'i';
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Character");
+
+        char ch=sc.next().charAt(0);
         if(ch == 'a'|| ch == 'i'|| ch == 'e'||ch=='o'||ch =='u'){
             System.out.println(ch + " is vowel");
         }
